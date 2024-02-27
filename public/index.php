ba534dc5d0fca5e52d396e16e670b7fbf000886c
Pratham Kumar <pratham@appzroot.com>
2024-02-27 15:00:19 +0530

    database setuped

--- public/index.php
+++ public/index.php
@@ -2,8 +2,12 @@
 
 require "../helpers.php";
 require basePath('Router.php');
+require basePath('Database.php');
+
+$config = require basePath('config/db.php');
 
 
+$db = new Database($config);
 
 $router = new Router();
 $routes = require basePath('routes.php');
