f3d8dc8c675b67db39d77c204a33a10e14fdc223
Pratham Kumar <prathamonweb@gmail.com>
2024-03-14 19:00:31 +0530

    basic class autolader

--- public/index.php
+++ public/index.php
@@ -1,22 +1,29 @@
 <?php
 
+require __DIR__.'/../vendor/autoload.php';
 require "../helpers.php";
-require basePath('Framework/Router.php');
-require basePath('Framework/Database.php');
 
-
-
+/**
+ *  Loading Classes
+ */
 spl_autoload_register(function ($class) {
-
+    $path=basePath("Framework/".$class.".php");
+    if(file_exists($path)){
+        require $path;
+    }
 });
 
+// Instantiate The Router
 $router = new Router();
+
+// Get Routes
 $routes = require basePath('routes.php');
 
+// Get Current URI & Method
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $method = $_SERVER['REQUEST_METHOD'];
 
-
+// Route the request
 $router->route($uri, $method);
 
 
